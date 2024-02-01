4725e657c6c2b90fa2a2fb56f8df529cbf5804e1
Bensu Beğenilmiş <bensubegenilmiss@gmail.com>
2024-02-01 16:15:02 +0300

    css

---
+++ screens/LoginScreen.js
@@ -0,0 +1,52 @@
+import { StyleSheet, Text, View , KeyboardAvoidingView, TextInput, TouchableOpacity} from 'react-native';
+import React from 'react';
+
+export default function LoginScreen() {
+  return (
+    <KeyboardAvoidingView
+    style = {styles.container}
+    behavior = 'padding'
+    >
+        
+      <View style = {styles.inputContainer}>
+        <TextInput style = {styles.input}
+         placeholder='Email'/>
+
+        <TextInput style = {styles.input}
+        placeholder='Şifre'/>
+      </View>
+      <View style = {styles.buttonContainer}>
+        <TouchableOpacity style = {styles.button}>
+            <Text style = {styles.buttonText}>Giriş Yap</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style = {[styles.button, styles.outlineButton]}>
+            <Text style = {styles.outlineButtonText}>Kayıt Ol</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    inputContainer:{
+        width:'80%',
+        
+    },
+    input:{
+        backgroundColor:'white',
+        paddingHorizontal:
+    },
+    buttonContainer:{},
+    button:{},
+    buttonText:{},
+    outlineButton:{},
+    outlineButtonText:{},
+
+
+
+});
