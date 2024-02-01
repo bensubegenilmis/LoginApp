0d3eb8310749a5fa94fffa8bd1de6555a654c08d
Bensu Beğenilmiş <bensubegenilmiss@gmail.com>
2024-02-01 16:41:42 +0300

    login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View , KeyboardAvoidingView, TextInput, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
 
 export default function LoginScreen() {
@@ -6,78 +6,78 @@ export default function LoginScreen() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-  return (
-    <KeyboardAvoidingView
-    style = {styles.container}
-    behavior = 'padding'
-    >
-        
-      <View style = {styles.inputContainer}>
-        <TextInput style = {styles.input}
-         placeholder='Email'
-         value={email}
-         onChangeText={text => setEmail(text)}
-         />
+    return (
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior='padding'
+        >
 
-        <TextInput style = {styles.input}
-        placeholder='Şifre' 
-        value={password}
-        onChangeText={text => setPassword(text)}
-        secureTextEntry/>
-      </View>
-      <View style = {styles.buttonContainer}>
-        <TouchableOpacity style = {styles.button}>
-            <Text style = {styles.buttonText}>Giriş Yap</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style = {[styles.button, styles.outlineButton]}>
-            <Text style = {styles.outlineButtonText}>Kayıt Ol</Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAvoidingView>
-  );
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.input}
+                    placeholder='Email'
+                    value={email}
+                    onChangeText={text => setEmail(text)}
+                />
+
+                <TextInput style={styles.input}
+                    placeholder='Şifre'
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    secureTextEntry />
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Giriş Yap</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.button, styles.outlineButton]}>
+                    <Text style={styles.outlineButtonText}>Kayıt Ol</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
+    );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    inputContainer:{
-        width:'80%',
-        
-    },
-    input:{
-        backgroundColor:'white',
-        paddingHorizontal: 15,
-        paddingVertical:10,
-        marginTop:5,
-        borderRadius:10,
-    },
-    buttonContainer:{
-        width:'50%',
-        marginTop:20,
-    },
-    button:{
-        backgroundColor:'deepskyblue',
-        padding:15,
-        alignItems:'center',
-        borderRadius:10,
-    },
-    buttonText:{
-        color:'white',
-        fontSize:18,
-        fontWeight:'700',
+    inputContainer: {
+        width: '80%',
 
     },
-    outlineButton:{
-        backgroundColor:'white',
-        marginTop:5,
+    input: {
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        marginTop: 5,
+        borderRadius: 10,
     },
-    outlineButtonText:{
-        fontSize:18,
-        fontWeight:'700',
-        color:'deepskyblue'
+    buttonContainer: {
+        width: '50%',
+        marginTop: 20,
+    },
+    button: {
+        backgroundColor: 'deepskyblue',
+        padding: 15,
+        alignItems: 'center',
+        borderRadius: 10,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: '700',
+
+    },
+    outlineButton: {
+        backgroundColor: 'white',
+        marginTop: 5,
+    },
+    outlineButtonText: {
+        fontSize: 18,
+        fontWeight: '700',
+        color: 'deepskyblue'
     },
 
 
